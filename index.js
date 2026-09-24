7c08052d8ceeba64fe8a707e6a9ee37f6aeeac01
laura <laha1003@stud.ek.dk>
2026-09-24 19:56:48 +0200

    Kategorier og Sæsoner gjort dynamisk vba. fetch + innerhtml

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 "use strict";
-console.log("yo");
 const productUrl = "https://kea-alt-del.dk/t7/api/categories";
+const categories = document.querySelector("#categories_list");
+const seasonsUrl = "https://kea-alt-del.dk/t7/api/seasons";
+const seasons = document.querySelector("#seasons_list");
 
+//Category
 function getData() {
   fetch(productUrl).then((result) => result.json().then((data) => showData(data)));
 }
+
 function showData(data) {
-  console.log(data);
+  // console.log(data);
+  categories.innerHTML = "";
+  let catInnerHTML = "";
+  data.forEach((element) => {
+    catInnerHTML += `<a class="display" href="produktliste.html">${element.category}</a>`;
+  });
+  categories.innerHTML = catInnerHTML;
 }
 
 getData();
+
+//Seasons
+function getDataSeasons() {
+  fetch(seasonsUrl).then((result) => result.json().then((data) => showDataSeasons(data)));
+}
+
+function showDataSeasons(data) {
+  // console.log(data);
+  seasons.innerHTML = "";
+  let seaInnerHTML = "";
+  data.forEach((element) => {
+    seaInnerHTML += `<a class="display" href="produktliste.html">${element.season}</a>`;
+  });
+  seasons.innerHTML = seaInnerHTML;
+}
+
+getDataSeasons();
